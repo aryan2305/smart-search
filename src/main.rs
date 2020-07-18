62da796855dfe3ebbe7602266ee79fe52b17bd1d
Aryan Agrawal <aryanpranshu@gmail.com>
2020-07-19 01:42:41 +0530

    Parsing commands and redirecting to specific urls

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,47 @@ fn index() -> &'static str {
 }
 
 #[get("/search?<cmd>")]
-fn search(cmd: String) -> &'static str {
+fn search(cmd: String) -> Redirect {
     println!("You typed in: {}", cmd);
-    "Hello from the search page!"
+    let command = get_command_from_query_string(&cmd);
+
+    let redirect_url = match command {
+        "tw" => String::from("https://twitter.com"),
+    _ => String::from("https://google.com")
+    };
+    Redirect::to(redirect_url)
 }
 
 fn main() {
     rocket::ignite().mount("/", routes![index, search]).launch();
+}
+
+fn get_command_from_query_string(query_string: &str) -> &str {
+    if query_string.contains(' ') {
+        // We need to this to know where to slice the string
+        let index_of_space = query_string.find(' ').unwrap_or(0);
+        return &query_string[..index_of_space];
+    }
+    // Otherwise, return the query string as is
+    query_string
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_command_from_query_string_no_whitespace() {
+        // Test with command only
+        let actual = get_command_from_query_string("tw");
+        let expected = "tw";
+        assert_eq!(actual, expected);
+    }
+    #[test]
+    fn test_get_command_from_query_string_with_whitespace() {
+        let actual = get_command_from_query_string
+        ("tw @fbOpenSource");
+        let expected = "tw";
+        assert_eq!(actual, expected);
+    }
 }
